1ffc1cc23243f470403bf5e0cd6975bffea10de3
Brenno Almeida <brenno1812almeida@gmail.com>
2023-09-27 21:09:11 -0300

    content created

---
+++ src/app/pages/data/dataFake.ts
@@ -0,0 +1,6 @@
+export const dataFake = [{
+  "id": "1",
+  "title":"Saiu a nova versão do angular",
+  "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus rerum minima facere at, reiciendis autem, eligendi labore quae voluptatibus deleniti voluptatum corrupti veritatis quidem doloremque maiores veniam. Velit, dolorum deleniti.",
+  "photoCover": "https://pipocamoderna.com.br/storage/2019/08/avengers-endgame-iron-man-snap-1180646-1280x0.jpeg"
+}];
